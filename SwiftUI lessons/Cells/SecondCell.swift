601f739703ab4ddd927260200f6684e123133ef7
Anton SVTSV <freshbreathe@gmail.com>
2021-12-06 18:46:16 +0300

    Add preparations

---
+++ SwiftUI lessons/Cells/SecondCell.swift
@@ -0,0 +1,22 @@
+//
+//  SecondCell.swift
+//  SwiftUI lessons
+//
+//  Created by Антон Сивцов on 06.12.2021.
+//
+
+import SwiftUI
+
+struct SecondCell: View {
+    
+    var body: some View {
+        Text("second cell")
+    }
+    
+}
+
+struct SecondCell_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondCell()
+    }
+}
---
+++ SwiftUI lessons/Cells/ThirdCell.swift
@@ -0,0 +1,22 @@
+//
+//  ThirdCell.swift
+//  SwiftUI lessons
+//
+//  Created by Антон Сивцов on 06.12.2021.
+//
+
+import SwiftUI
+
+struct ThirdCell: View {
+    
+    var body: some View {
+        Text("third cell")
+    }
+    
+}
+
+struct ThirdCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdCell()
+    }
+}
