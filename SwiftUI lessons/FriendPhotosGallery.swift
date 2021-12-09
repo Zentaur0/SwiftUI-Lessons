410786eef46a4f74f3a06be19aba6ef0d1688df3
Anton SVTSV <freshbreathe@gmail.com>
2021-12-09 19:09:52 +0300

    Add preparations

---
+++ SwiftUI lessons/FriendPhotosGallery.swift
@@ -0,0 +1,20 @@
+//
+//  FriendPhotosGallery.swift
+//  SwiftUI lessons
+//
+//  Created by Антон Сивцов on 09.12.2021.
+//
+
+import SwiftUI
+
+struct FriendPhotosGallery: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FriendPhotosGallery_Previews: PreviewProvider {
+    static var previews: some View {
+        FriendPhotosGallery()
+    }
+}
