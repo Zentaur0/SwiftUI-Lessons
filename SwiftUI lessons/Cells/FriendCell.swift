985923ccf19ff34fee0fbb4163581ad8a43abdb8
Anton SVTSV <freshbreathe@gmail.com>
2021-12-28 16:14:01 +0300

    Add animation for friend avatar

--- SwiftUI lessons/Cells/FriendCell.swift
+++ SwiftUI lessons/Cells/FriendCell.swift
@@ -13,6 +13,9 @@ struct FriendCell: View {
     // MARK: - Properties
     private let imageMargin: CGFloat = 50
     private let friend: Friend
+    private let imageAnimation = Animation.easeIn(duration: 1.5)
+    @State private var isImageDamping = false
+    @State private var isInitialPoint = false
     
     // MARK: - Init
     init(friend: Friend) {
@@ -27,6 +30,14 @@ struct FriendCell: View {
                 .frame(width: imageMargin, height: imageMargin, alignment: .leading)
                 .cornerRadius(25)
                 .shadow(color: .gray, radius: 2, x: 2, y: 2)
+                .scaleEffect(isImageDamping ? 2 : 1)
+                .scaleEffect(isInitialPoint ? 0.5 : 1)
+                .animation(.interpolatingSpring(stiffness: 200, damping: 3),
+                           value: isImageDamping)
+                .onTapGesture {
+                    isImageDamping.toggle()
+                    isInitialPoint.toggle()
+                }
             
             VStack(alignment: .leading, spacing: 10) {
                 Text(friend.name + " " + friend.lastName)
